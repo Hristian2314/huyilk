ce86d870f0e56f18f35086963857f51bf690258d
Hristian2314 <f75@abv.bg>
2024-02-21 08:56:07 +0200

    dfg

---
+++ zadacha/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace zadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kapacitet = 255;
+
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int otnovo = int.Parse((Console.ReadLine()));
+
+
+            }
+
+
+
+        }
+    }
+}
+
